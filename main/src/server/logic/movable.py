25b1e2f9fdce851290d45e5d546fe6d0cabdf4bd
Lysak Tatiana <lysak-tatiana2501@ya.ru>
2025-02-01 23:40:46 +0300

    feature: adding homework 2

---
+++ main/src/server/logic/movable.py
@@ -0,0 +1,33 @@
+import zope
+
+from server.errors import MoveParamException
+from server.interfaces.movable import IMovable
+from server.models import Vector
+
+
+@zope.interface.implementer(IMovable)
+class Move:
+    """Движение объекта"""
+
+    def __init__(self, movable):
+        self.movable = movable
+
+    def get_position(self) -> Vector:
+        return self.movable.position
+
+    def get_velocity(self) -> Vector:
+        return self.movable.velocity
+
+    def set_position(self, new_position: Vector) -> None:
+        self.movable.position = new_position
+
+    def execute(self) -> None:
+        """Установка новой позиции"""
+
+        if not isinstance(self.movable.position, Vector):
+            raise MoveParamException('Incorrect position value')
+
+        if not isinstance(self.movable.velocity, Vector):
+            raise MoveParamException('Incorrect velocity value')
+
+        self.set_position(self.get_position() + self.get_velocity())
